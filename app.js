ee14b3b635d2c757f6d6eaca8051d64d4811b962
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2019-02-13 20:21:57 -0800

    console.log on query parameter

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello from across the table!!'));
+app.get('/hello', (req, res) => {
+    
+    console.log(req.query);
+    res.send('World!')
+    
+});
+
+
+app.get('/add', (req, res) => res.send(0));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
